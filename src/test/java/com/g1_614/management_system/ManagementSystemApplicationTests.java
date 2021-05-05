cc0c482791227919d175ec25382240b9085e0616
xws <wansenxu@163.com>
2021-05-05 17:12:07 +0800

    origin

---
+++ src/test/java/com/g1_614/management_system/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g1_614.management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
